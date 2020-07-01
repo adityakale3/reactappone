228659651c3262e68f44795ce0935aae4d6fc212
Aditya <adityakale3@gmail.com>
2020-07-01 16:13:29 +0530

    My First App

---
+++ myreactnpxapp/src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+
+function Footer(){
+    return  (<div  className="parentElement">
+                <h5 className="childElement">Hi !, I am Chandler Bing.</h5>
+                <p className="childElement"> I am pretty Famous, <b>Wordlwide</b></p>
+            </div>);
+}
+
+
+export default Footer
